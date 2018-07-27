93df794463b753cadba38c1b17c588aeec534794
Rudi Yardley <contact@rudiyardley.com>
2018-07-27 21:11:41 +0700

    Remove old files

--- dist/createMessageClient.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function createMessageClient(...middleware) {
-    if (!middleware || middleware.length === 0) {
-        throw new Error('No middleware provided to message client.');
-    }
-}
-exports.default = createMessageClient;
-//# sourceMappingURL=createMessageClient.js.map
