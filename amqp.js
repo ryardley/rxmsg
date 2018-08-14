604d81750613291c0e379d6e56996dfba5ddda10
Rudi Yardley <contact@rudiyardley.com>
2018-08-14 09:58:40 +0700

    Fix pathing issue

--- amqp.js
+++ amqp.js
@@ -1,4 +1,4 @@
 // Allow deep linking for the amqp module
 // This file is NOT generated
-var amqp = require('./dist/middleware/amqp');
+var amqp = require('./dist/src/middleware/amqp');
 exports.createAmqpConnector = amqp.default;
