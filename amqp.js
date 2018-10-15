de1b01a6de5bb4037417e147f4d6b9fd531dc1fc
Rudi Yardley <contact@rudiyardley.com>
2018-10-15 11:27:40 +0800

    Enable amqp connector

--- amqp.js
+++
@@ -1,4 +0,0 @@
-// Allow deep linking for the amqp module
-// This file is NOT generated
-var amqp = require('./dist/src/middleware/amqp');
-exports.createAmqpConnector = amqp.default;
---
+++ src/amqp.ts
@@ -0,0 +1,1 @@
+export { default as createAmqpConnector } from './middleware/amqp';
