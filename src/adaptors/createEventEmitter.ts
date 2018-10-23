2bed663dc2a70ab6617c1c64dd22a864e9af36e9
Rudi Yardley <contact@rudiyardley.com>
2018-10-23 08:53:05 +0800

    Event emitter optional props and guards(#10)

--- src/adaptors/createEventEmitter.ts
+++ src/adaptors/createEventEmitter.ts
@@ -5,19 +5,26 @@ import { createProducer } from '../endpoints/producer';
 import { Middleware } from '../types';
 type CallBackFn = (payload: any) => void;
 class RxMsgEventEmitter<T, P> {
-  private producer: Observer<{ to: string; body: T }>;
-  private consumer: Observable<{ to: string; body: P }>;
+  private producer: Observer<{ to: string; body: T }> | void;
+  private consumer: Observable<{ to: string; body: P }> | void;
   private subscriptions: { [k: string]: Map<CallBackFn, Subscription> } = {};
 
   constructor(
-    sender: Middleware<{ to: string; body: T }>,
-    receiver: Middleware<{ to: string; body: P }>
+    sender: Middleware<{ to: string; body: T }> | undefined,
+    receiver: Middleware<{ to: string; body: P }> | undefined
   ) {
-    this.producer = createProducer<{ to: string; body: T }>(sender);
-    this.consumer = createConsumer<{ to: string; body: P }>(receiver);
+    if (sender) {
+      this.producer = createProducer<{ to: string; body: T }>(sender);
+    }
+    if (receiver) {
+      this.consumer = createConsumer<{ to: string; body: P }>(receiver);
+    }
   }
 
   public on = (eventName: string, callback: CallBackFn) => {
+    if (!this.consumer) {
+      throw new Error('Cannot use on(). Consumer was not provided');
+    }
     const subscription = this.consumer
       .pipe(filter(m => m.to === eventName))
       .subscribe(msg => {
@@ -30,17 +37,22 @@ class RxMsgEventEmitter<T, P> {
   public off = (eventName: string, callback: CallBackFn) => {
     const subscription = this.subscriptions[eventName].get(callback);
     if (subscription) {
+      this.subscriptions[eventName].delete(callback);
       subscription.unsubscribe();
     }
   };
+
   public emit = (to: string, body: T) => {
+    if (!this.producer) {
+      throw new Error('Cannot use emit(). Producer was not provided');
+    }
     this.producer.next({ body, to });
   };
 }
 
 export default function createEventEmitter<T, P>(o: {
-  sender: Middleware<{ to: string; body: T }>;
-  receiver: Middleware<{ to: string; body: P }>;
+  sender?: Middleware<{ to: string; body: T }>;
+  receiver?: Middleware<{ to: string; body: P }>;
 }): RxMsgEventEmitter<T, P> {
   return new RxMsgEventEmitter(o.sender, o.receiver);
 }
