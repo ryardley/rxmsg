8f624c99b8ad043d6426f7b1a5d9a9b8d32dc07d
Rudi Yardley <contact@rudiyardley.com>
2018-10-29 20:48:09 +0800

    Make each loopback independent

--- src/middleware/loopback/index.ts
+++ src/middleware/loopback/index.ts
@@ -13,32 +13,28 @@ type ReceiverConfig = {
   route: string;
 };
 
-function createReceiver<P extends IMessage>(
-  route?: string,
-  persistFn?: PersistFn
-) {
-  return () =>
-    receiveStream.asObservable().pipe(
-      persistFn ? tap(persistFn) : identity,
-      route ? filter((m: P) => m.to === route) : identity
-    );
-}
-
-// Forward messages
-const createSender = <T extends IMessage>(config: LoopBackConfig) => (
-  sendStream: Observable<T>
-) => {
-  const delayAmount = config.delay || 0;
-  // Fork the stream to receive
-  sendStream
-    .pipe(delayAmount ? delay(delayAmount) : identity)
-    .subscribe(receiveStream);
-  return sendStream;
-};
-
 export default function createConnector<T extends IMessage, P extends IMessage>(
   config: LoopBackConfig = {}
 ): Connector<T, P> {
+  function createReceiver(route?: string, persistFn?: PersistFn) {
+    return () =>
+      receiveStream.asObservable().pipe(
+        persistFn ? tap(persistFn) : identity,
+        route ? filter((m: P) => m.to === route) : identity
+      );
+  }
+
+  // Forward messages
+  const createSender = (loopBackConfig: LoopBackConfig) => (
+    sendStream: Observable<T>
+  ) => {
+    const delayAmount = loopBackConfig.delay || 0;
+    // Fork the stream to receive
+    sendStream
+      .pipe(delayAmount ? delay(delayAmount) : identity)
+      .subscribe(receiveStream);
+    return sendStream;
+  };
   return {
     receiver: (options?: ReceiverConfig) =>
       createReceiver(options && options.route, config.persist),
